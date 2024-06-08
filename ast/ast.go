44c81eb0bbb4140b6666ac11c4878c105e166e4c
arty <artemtiunelis@gmail.com>
2024-06-08 22:10:07 +0200

    started adding function parsing

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"github.com/artylemon/monkey_interpreter/token"
+	"strings"
 )
 
 type Node interface {
@@ -203,4 +204,29 @@ func (bs *BlockStatement) String() string {
 	}
 	return out.String()
 
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
 }
