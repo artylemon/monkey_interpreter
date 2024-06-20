a02763c6d112a21e6c3c59444fa970387c7d90fc
Artyom <artemtiunelis@gmail.com>
2024-06-20 06:24:38 +0100

    added evaluation to REPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/artylemon/monkey_interpreter/evaluator"
 	"github.com/artylemon/monkey_interpreter/lexer"
 	"github.com/artylemon/monkey_interpreter/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
