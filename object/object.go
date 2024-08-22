2e7d3cc40b377b9a3df6ae28886fb120351e3162
Artyom <artemtiunelis@gmail.com>
2024-08-22 16:55:25 +0100

    Start to implement hashmaps

--- object/object.go
+++ object/object.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"github.com/artylemon/monkey_interpreter/ast"
+	"hash/fnv"
 	"strings"
 )
 
@@ -117,4 +118,30 @@ func (ao *Array) Inspect() string {
 	out.WriteString("]")
 
 	return out.String()
+}
+
+type HashKey struct {
+	Type  ObjectType
+	Value uint64
+}
+
+func (b *Boolean) HashKey() HashKey {
+	var value uint64
+	if b.Value {
+		value = 1
+	} else {
+		value = 0
+	}
+
+	return HashKey{Value: value, Type: b.Type()}
+}
+
+func (i *Integer) HashKey() HashKey {
+	return HashKey{Type: i.Type(), Value: uint64(i.Value)}
+}
+
+func (s *String) HashKey() HashKey {
+	h := fnv.New64a()
+	h.Write([]byte(s.Value))
+	return HashKey{Type: s.Type(), Value: h.Sum64()}
 }
